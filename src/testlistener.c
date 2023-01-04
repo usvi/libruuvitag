282a5a03c7063f3ac45869eab12b5240aa2c9b9d
Janne Paalijarvi <janne.paalijarvi@efore.com>
2023-01-04 19:14:31 +0200

    Added valgrind target

--- src/testlistener.c
+++ src/testlistener.c
@@ -13,7 +13,7 @@ int main()
   printf("Ruuvitag testlistener starting\n");
   u8LibRuuviTagInit("+CURRENT+NEW", "+CURRENT+NEW");
 
-  for(u8_i = 30; u8_i > 0; u8_i--)
+  for(u8_i = 3; u8_i > 0; u8_i--)
   {
     printf("Testlistener sleeping %" PRIu8 "\n", u8_i);
     sleep(1);
