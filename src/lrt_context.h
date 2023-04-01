7cfa14ef38baa4ec427b142fa02b58393948bb09
Janne Paalijarvi <janne.paalijarvi@efore.com>
2023-04-01 17:52:17 +0300

    Almost (dunno why is this so hard)

--- src/lrt_context.h
+++ src/lrt_context.h
@@ -4,18 +4,22 @@
 #include <semaphore.h>
 #include <dbus/dbus.h>
 
-typedef struct 
+typedef struct lrt_dbus_type lrt_dbus_type;
+typedef struct lrt_context_type lrt_context_type;
+
+
+struct lrt_dbus_type
 {
   DBusConnection* px_sys_conn;
 
-} lrt_dbus_type;
+};
 
 
-typedef struct
+struct lrt_context_type
 {
   sem_t x_shared_data_semaphore;
   lrt_dbus_type* px_dbus;
-} lrt_context_type;
+};
 
 
 #endif // #ifndef _LRT_CONTEXT_H_
