c69f3388cae904fdb9fdd6d668a91274d5a4d361
Janne Paalijarvi <janne.paalijarvi@efore.com>
2023-07-09 01:51:59 +0300

    Comparmentalized context

--- src/libruuvitag.h
+++ src/libruuvitag.h
@@ -1,33 +1,35 @@
 #ifndef _LIBRUUVITAG_H_
-#define _LIBRUUVITAG_H__
+#define _LIBRUUVITAG_H_
 
+
+// Assert proper backend and include corresponding header
 #ifdef BACKEND_LINUX_DBUS_BLUEZ
 #include "lrt_linux_dbus_bluez.h"
 #else
 #error Invalid backend
 #endif
 
+
+// Regular headers
 #include <stdint.h>
 
-#define LIBRUUVITAG_RES_OK    (0)
-#define LIBRUUVITAG_RES_AGAIN (1)
-#define LIBRUUVITAG_RES_FATAL (2)
 
-
+// Common context definitons
 typedef struct libruuvitag_context_type libruuvitag_context_type;
 
 struct libruuvitag_context_type
 {
   // Common structures
+
+  // Backend specific structures
 #ifdef BACKEND_LINUX_DBUS_BLUEZ
-  lrt_ldb_context_type ldb_context;
+  lrt_ldb_context_type x_ldb_context; // Linux Dbus Bluez
 #endif
 };
 
 
+// Function prototypes
 libruuvitag_context_type* pxLibRuuviTagInit(char* s_listen_on_bt_adapters, char* s_listen_to_ruuvitags);
-
 void vLibRuuviTagDeinit(libruuvitag_context_type* px_context);
 
 #endif // #define _LIBRUUVITAG_H_
-
--- src/lrt_linux_dbus_bluez.h
+++ src/lrt_linux_dbus_bluez.h
@@ -1,16 +1,27 @@
 #ifndef _LRT_LINUX_DBUS_BLUEZ_H_
 #define _LRT_LINUX_DBUS_BLUEZ_H_
 
+
+// Dbus-related definitions to populate local structure
 #include <dbus/dbus.h>
 
-typedef struct lrt_ldb_context_type lrt_ldb_context_type;
 
+// Careful definition ordering to keep top-level header
+// happy and at the same time define strictly the dbus-
+// related stuff here.
+typedef struct lrt_ldb_context_type lrt_ldb_context_type;
 
 struct lrt_ldb_context_type
 {
 
 };
 
+
+// Top-level header
+#include "libruuvitag.h"
+
+
+// Function prototypes
 
 #endif // #ifndef _LRT_LINUX_DBUS_BLUEZ_H_
 
